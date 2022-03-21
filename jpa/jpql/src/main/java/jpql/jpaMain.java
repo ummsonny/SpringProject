a626e5e7c99f70db732ff0741f612773b0628cb9
ummsonny <zhtkdzjtkd@naver.com>
2022-03-21 20:32:12 +0900

    init4

---
+++ jpa/jpql/src/main/java/jpql/jpaMain.java
@@ -0,0 +1,31 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class jpaMain {
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); //persistence.xml에 이름 설정해놈
+
+        EntityManager entityManager = emf.createEntityManager();
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin(); // 트랜잭션은 db연결하는 것이라고 보면된다.
+
+        try{
+
+            tx.commit();
+
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            entityManager.close();
+        }
+
+
+        emf.close();
+    }
+}
+
